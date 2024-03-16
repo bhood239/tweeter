4d58d81bf6fda167914fac7825fd577120a5704a
Ben Hood <benhood97@btinternet.com>
2024-03-16 15:31:37 -0700

    refactor per feedback

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,15 @@
+
 $(document).ready(function() {
   $( ".tweet-text" ).on( "input", function( event ) {
     const maxLength = 140;
     const currentLength = $(this).val().length;
     const remaining = maxLength - currentLength;
-    $(".counter").text(remaining);
+    const $counter = $(this).closest('form').find('.counter');
+    $counter.text(remaining);
     if (remaining < 0) {
-      $(".counter").css('color', '#ff0000');
+      $counter.css('color', '#ff0000');
     } else {
-      $(".counter").css('color', '');
+      $counter.css('color', '');
     }
   })
 })
