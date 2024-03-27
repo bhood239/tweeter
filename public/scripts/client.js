eb03bd161fa7703ab4957030fc71aebff4f0cada
Ben Hood <benhood97@btinternet.com>
2024-03-26 20:23:55 -0700

    refactor tweet-container

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -11,31 +11,33 @@ $(document).ready(function() {
     const formattedDate = timeago.format(tweetObject.created_at);
 
     const $tweet = $(`
-        <header class="current-tweets-top">
-          <div class="top-left">
-            <img class="current-profile-pic" src="${tweetObject.user.avatars}"> 
-            <div>${tweetObject.user.name}</div>
-          </div>
-          <div class="top-right">${tweetObject.user.handle}</div>
-        </header>
-        <footer>
-          <p></p>
-          <div class="bottom">
-            <div>${formattedDate}</div>
-            <div>
-              <i class="fa-solid fa-flag"></i>
-              <i class="fa-solid fa-repeat"></i>
-              <i class="fa-solid fa-heart"></i>
+        <div class="tweet-whole">
+          <header class="current-tweets-top">
+            <div class="top-left">
+              <img class="current-profile-pic" src="${tweetObject.user.avatars}"> 
+              <div>${tweetObject.user.name}</div>
             </div>
-          </div>
-        </footer>
+            <div class="top-right">${tweetObject.user.handle}</div>
+          </header>
+          <footer>
+            <p></p>
+            <div class="bottom">
+              <div>${formattedDate}</div>
+              <div>
+                <i class="fa-solid fa-flag"></i>
+                <i class="fa-solid fa-repeat"></i>
+                <i class="fa-solid fa-heart"></i>
+              </div>
+            </div>
+          </footer>
+        </div>
     `);
 
     $tweet.find("p").text(tweetObject.content.text);
     return $tweet;
   };
 
-  
+
   // Function to render tweets
   const renderTweets = function(tweets) {
     $('.tweets-container').empty(); // Clear existing tweets
